ecd1835ec3fa9fd90e170f8b0f1db25027ce17b8
saharsh <saharshkhalokar14@gmail.com>
2026-01-02 23:02:50 +0530

    day2-dp

---
+++ DP/1D DP/Climbing stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int solve(int n,int i,vector<int>& dp){
+        if(i==n) return 1;
+        else if(i>n) return 0;
+
+        if(dp[i]!=-1) return dp[i];
+
+        return dp[i] = solve(n,i+1,dp) + solve(n,i+2,dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,0,dp);
+    }
+};
